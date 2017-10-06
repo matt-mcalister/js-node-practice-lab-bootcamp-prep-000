a1231eb8811e5d73e41ab7c7d47bbc317fb5a1f6
Matt McAlister <matt.mcalister93@gmail.com>
2017-10-06 20:18:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,9 @@
 var name = "Matt"
 var height = "74"
 
-height = console.log(parseInt(height)+1)
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(parseInt(height)+1)
